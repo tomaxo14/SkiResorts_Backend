f3d58b0f14162b7bbe15396f40739b7e2c3750e7
tomaxo14 <tomek.przybylski@onet.pl>
2020-09-25 20:10:10 +0200

    fix location entity

--- src/main/java/com/example/skiResorts/entities/Location.java
+++ src/main/java/com/example/skiResorts/entities/Location.java
@@ -1,10 +1,12 @@
 package com.example.skiResorts.entities;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+@NoArgsConstructor
 @Setter
 @Getter
 @Document(collection = "locations")
